ef88359a538d0856ad6b3b416531add973e95835
Rajat Baraiya <baraiya.rajat@gmail.com>
2021-01-21 13:16:09 +0530

    Update settings.py

--- NumberRecognizer/settings.py
+++ NumberRecognizer/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h*$unytg84m+ltyc)95)em&h6+2k7kd#q7e5v7k@kwyl9i@gmw'
+SECRET_KEY = '<Your Django Key Here!>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
